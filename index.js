59bb9007f7082a68d9d8ed9daeab3c434e3e7529
Stephen <stephen@agilestyle.com>
2015-12-07 11:16:50 -0700

    Use .env for twitter api keys.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').load();
+
 var Twit = require('twit');
 
 var T = new Twit({
-  consumer_key:        '...',
-  consumer_secret:     '...',
-  access_token:        '...',
-  access_token_secret: '...'
+  consumer_key:        'process.env.TWITTER_CONSUMER_KEY',
+  consumer_secret:     'process.env.TWITTER_CONSUMER_SECRET',
+  access_token:        'process.env.TWITTER_ACCESS_TOKEN',
+  access_token_secret: 'process.env.TWITTER_ACCESS_TOKEN_SECRET',
 });
